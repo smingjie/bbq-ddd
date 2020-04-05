e71160110e8dd6e832299ff7bf16ed9c022ee373
smj <maijie610@163.com>
2020-04-05 20:42:09 +0800

    try mp generator

---
+++ src/main/java/com/microserv/bbq/generator/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.microserv.bbq.generator.common;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 实体父类
+ *
+ * @author jockeys
+ * @since 2020/4/5
+ */
+@Data
+public class BaseEntity {
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
+
+    @ApiModelProperty(value = "创建人")
+    private String createBy;
+
+    @ApiModelProperty(value = "更新人")
+    private String updateBy;
+}
---
+++ src/main/java/com/microserv/bbq/infrastructure/general/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.microserv.bbq.infrastructure.general.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger2 configuration
+ *
+ * @author jockeys
+ * @since 2020/4/4
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String SWAGGER_SCAN_BASE_PACKAGE = "com.microserv.bbq.apis.controller";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DDD微服务框架")
+                .description("BBQ-DDD")
+                .termsOfServiceUrl("http://bbq.microserv.com")
+                .version("1.0.0")
+                .build();
+    }
+
+}
