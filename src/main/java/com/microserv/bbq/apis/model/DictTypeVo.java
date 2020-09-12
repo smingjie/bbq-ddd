54d7a952f552c3154ca38540d7c1bd14c9e0287c
smj <maijie610@163.com>
2020-09-13 03:09:37 +0800

    优化字典领域，添加工作流配置领域部分内容

--- src/main/java/com/microserv/bbq/apis/model/DictTypeVo.java
+++ src/main/java/com/microserv/bbq/apis/model/DictTypeVo.java
@@ -2,10 +2,12 @@ package com.microserv.bbq.apis.model;
 
 import com.google.common.collect.Lists;
 import com.microserv.bbq.domain.model.dict.DictAgg;
+import com.microserv.bbq.domain.model.dict.DictEntity;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -15,28 +17,31 @@ import java.util.List;
  * @since 2020/4/15
  */
 @Data
-public class DictTypeVo {
-    private String type;
-    private String name;
-    private List<DictItem> items;
+@NoArgsConstructor
+public class DictTypeVO {
+	private String type;
+	private String name;
+	private List<DictItem> items;
 
-    @Data
-    @Accessors(chain = true)
-    public class DictItem {
-        private String id;
-        private String key;
-        private String value;
-    }
+	@Data
+	@Accessors(chain = true)
+	@NoArgsConstructor
+	public static class DictItem {
+		private String id;
+		private String key;
+		private String value;
 
-    /**
-     * todo 解析为传输对象
-     */
-    public DictTypeVo(@NotNull DictAgg agg) {
-        this.type = agg.getType();
-        this.name = agg.getName();
-        this.items = Lists.transform(agg.getItemList(), o -> new DictItem()
-                .setId(o.getId()).setKey(o.getCode()).setValue(o.getValue())
-        );
+		public DictItem(DictEntity e) {
+			this.setId(e.getId()).setKey(e.getCode()).setValue(e.getValue());
+		}
+	}
 
-    }
+	/**
+	 * todo 解析为传输对象
+	 */
+	public DictTypeVO(@NotNull DictAgg agg) {
+		this.type = agg.getType();
+		this.name = agg.getName();
+		this.items = Lists.transform(agg.getItemList(), DictItem::new);
+	}
 }
